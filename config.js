bfba8fbb6459b16f542a0792e93215357f13b5c6
a-n-g-i-e-r-i <tristan.angieri@gmail.com>
2022-01-27 12:53:27 -0500

    updated config for prod

--- config.js
+++ config.js
@@ -2,6 +2,15 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+const env = process.env.NODE_ENV || process.env.DEVELOPMENT;
+let googlePrivateKey;
+
+if (env === process.env.DEVELOPMENT) {
+  googlePrivateKey = process.env.GOOGLE_PRIVATE_KEY;
+} else {
+  googlePrivateKey = JSON.parse(process.env.GOOGLE_PRIVATE_KEY);
+}
+
 module.exports = {
   PORT: process.env.PORT,
   ZOOM_VERIFICATION_TOKEN: process.env.ZOOM_VERIFICATION_TOKEN,
@@ -9,7 +18,7 @@ module.exports = {
   GOOGLE_ATTENDANCE_SHEET_ID: process.env.GOOGLE_ATTENDANCE_SHEET_ID,
   GOOGLE_CLIENT_EMAIL: process.env.GOOGLE_CLIENT_EMAIL,
   GOOGLE_ACCOUNT_TYPE: process.env.GOOGLE_ACCOUNT_TYPE,
-  GOOGLE_PRIVATE_KEY: process.env.GOOGLE_PRIVATE_KEY,
+  GOOGLE_PRIVATE_KEY: googlePrivateKey,
   ZOOM_EVENTS: process.env.ZOOM_EVENTS.split(","),
   MEETING_IDS: process.env.MEETING_IDS.split(","),
 };
