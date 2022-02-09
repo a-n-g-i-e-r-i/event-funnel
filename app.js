914301c781da95aa88451e44bf10480e6d99532e
a-n-g-i-e-r-i <tristan.angieri@gmail.com>
2022-02-09 11:42:22 -0500

    updated server arch

---
+++ app.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const config = require("./config");
+const { appendRowToGSheet } = require("./helpers/appendRowToGSheet");
+const {
+  UnprocessableEntityError,
+  UnauthorizedRequestError,
+  InternalServerError,
+} = require("./helpers/customErrors");
+
+app.use(express.static("./app.js"));
+
+app.get("/", (req, res) => {
+  res.send("REQUEST RECEIVED");
+});
+
+app.post("/", bodyParser.raw({ type: "application/json" }), (req, res) => {
+  let body;
+  let meetingInfo;
+  let meetingId;
+  let participantEmail;
+  let joinTime;
+
+  try {
+    body = JSON.parse(req.body);
+    meetingInfo = body.payload.object;
+    meetingId = meetingInfo.id;
+    participantEmail = meetingInfo.participant.email;
+    joinTime = meetingInfo.participant.date_time;
+
+    if (!config.ZOOM_EVENTS.includes(`${body.event}`)) {
+      throw new UnprocessableEntityError(
+        `EVENT TYPE IS NOT SUPPORTED: ${body.event}`
+      );
+    } else if (config.MEETING_IDS.includes(`${meetingId}`) === false) {
+      throw new UnprocessableEntityError(
+        `MEETING ID IS NOT SUPPORTED: ${meetingId}`
+      );
+    } else if (!req.headers.authorization === config.ZOOM_VERIFICATION_TOKEN) {
+      throw new UnauthorizedRequestError("UNAUTHORIZED POST REQUEST");
+    } else {
+      try {
+        appendRowToGSheet(meetingId, joinTime, participantEmail);
+        res.send("SUCCESS");
+        console.log(
+          `\nSUCCESS: PARTICIPANT DATA APPENDED TO GOOGLE SHEET:\n
+        MEETING ID: ${meetingId}\n
+        PARTICIPANT EMAIL: ${participantEmail}\n
+        JOIN TIME: ${joinTime}`
+        );
+        res.status(200);
+      } catch (e) {
+        throw e;
+      }
+    }
+  } catch (e) {
+    if (e instanceof UnauthorizedRequestError) {
+      const m = `FORBIDDEN: ${e.message}`;
+      res.status(403).end(m);
+      console.log(m);
+    } else if (e instanceof UnprocessableEntityError) {
+      const m = `UNPROCESSABLE ENTITY: ${e.message}`;
+      res.status(422).send(m);
+      console.log(m);
+    } else if (e instanceof InternalServerError) {
+      const m = "INTERNAL SERVER ERROR";
+      res.status(500).send(m);
+      console.log(m, e.message);
+    } else {
+      const m = `BAD REQUEST: ${e.message}`;
+      res.status(400).send(m);
+      console.log(m);
+    }
+  }
+});
+
+module.exports = {
+  app,
+};
---
+++ server.js
@@ -0,0 +1,6 @@
+const { app } = require("./app");
+const config = require("./config");
+
+app.listen(config.PORT, () => {
+  console.log(`SERVER IS RUNNING ON ${config.PORT}`);
+});
