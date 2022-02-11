ba1421efceec9e17f7225f99e424b8005ee16cb8
a-n-g-i-e-r-i <tristan.angieri@gmail.com>
2022-02-10 19:31:42 -0500

    updated app.js and tests

--- src/app.test.js
+++ src/app.test.js
@@ -1,24 +1,26 @@
 const request = require('supertest');
+const { loggers } = require('winston');
 const { app } = require('./app');
 const config = require('./config');
 const { appendRowToGSheet } = require('./helpers/appendRowToGSheet');
+const { v4: uuidv4 } = require('uuid');
 
+const logger = config.LOGGER;
 jest.mock('./helpers/appendRowToGSheet', () => ({
   appendRowToGSheet: jest.fn().mockImplementation(() => {}),
 }));
 
 const successReqHeaders = { authorization: config.ZOOM_VERIFICATION_TOKEN };
 const successEventType = config.ZOOM_EVENTS[0];
-const successMeetingId = config.MEETING_IDS[0];
-
-const successReq = {
+const successMeetingId = parseInt(config.MEETING_IDS[0]);
+let mockRec = {
   event: successEventType,
   event_ts: 1626230691572,
   payload: {
     account_id: 'AAAAAABBBB',
     object: {
       id: successMeetingId,
-      uuid: '4444AAAiAAAAAiAiAiiAii==',
+      uuid: uuidv4(),
       host_id: 'x1yCzABCDEfg23HiJKl4mN',
       topic: 'Test Meeting',
       type: 8,
@@ -38,7 +40,7 @@ const successReq = {
   },
 };
 
-const nullBodyReq = null;
+const deepCopy = (obj) => JSON.parse(JSON.stringify(obj));
 
 beforeEach(() => {
   jest.clearAllMocks();
@@ -54,20 +56,116 @@ describe('GET /', () => {
 
 describe('POST /', () => {
   it('responds status code 200 given correct request body', async () => {
+    let tempMockRec = deepCopy(mockRec);
+
     const res = await request(app)
       .post('/')
       .set(successReqHeaders)
-      .send(successReq);
+      .send(tempMockRec);
+
     expect(res.status).toBe(200);
     expect(appendRowToGSheet).toHaveBeenCalledTimes(1);
   });
 
-  it('responds status code 400 given a null request body', async () => {
+  it('responds status code 422 given a null request body', async () => {
+    let tempMockRec = deepCopy(mockRec);
+    tempMockRec = null;
+
     const res = await request(app)
       .post('/')
       .set(successReqHeaders)
-      .send(nullBodyReq);
-    expect(res.status).toBe(400);
+      .send(tempMockRec);
+
+    expect(res.status).toBe(422);
     expect(appendRowToGSheet).toHaveBeenCalledTimes(0);
   });
+
+  it('responds status code 422 given a null payload object', async () => {
+    let tempMockRec = deepCopy(mockRec);
+    tempMockRec.payload.object = null;
+
+    const res = await request(app)
+      .post('/')
+      .set(successReqHeaders)
+      .send(tempMockRec);
+
+    expect(res.status).toBe(422);
+    expect(appendRowToGSheet).toHaveBeenCalledTimes(0);
+  });
+
+  it('responds status code 422 given a null meetingId', async () => {
+    let tempMockRec = deepCopy(mockRec);
+    tempMockRec.payload.object.id = null;
+
+    const res = await request(app)
+      .post('/')
+      .set(successReqHeaders)
+      .send(tempMockRec);
+
+    expect(res.status).toBe(422);
+    expect(appendRowToGSheet).toHaveBeenCalledTimes(0);
+  });
+
+  it('responds status code 422 given a null participantEmail', async () => {
+    let tempMockRec = deepCopy(mockRec);
+    tempMockRec.payload.object.participant.email = null;
+
+    const res = await request(app)
+      .post('/')
+      .set(successReqHeaders)
+      .send(mockRec);
+
+    expect(res.status).toBe(422);
+    expect(appendRowToGSheet).toHaveBeenCalledTimes(0);
+  });
+
+  // body = JSON.parse(req.body);
+  // meetingInfo = body.payload.object;
+  // meetingId = meetingInfo.id;
+  // participantEmail = meetingInfo.participant.email; <-- null/undefined
+  // joinTime = meetingInfo.participant.join_time;
+  // send BAD REQUEST 400
+
+  // describe("when joinTime is null/undefined", () => {
+  //   // body = JSON.parse(req.body);
+  //   // meetingInfo = body.payload.object;
+  //   // meetingId = meetingInfo.id;
+  //   // participantEmail = meetingInfo.participant.email;
+  //   // joinTime = meetingInfo.participant.join_time; <-- null/undefined
+  //   // send BAD REQUEST 400
+  // });
+
+  // describe("when ZOOM_EVENTS includes event", () => {
+  //   // should appendRowToGSheet (mock this)
+  //   // send SUCCESS 200
+  // });
+
+  // describe("when ZOOM_EVENTS does not include event", () => {
+  //   // throw new UnprocessableEntityError(
+  //   //   `EVENT TYPE IS NOT SUPPORTED: ${body.event}`
+  //   // );
+  //   // send UNPROCESSABLE ENTITY 422
+  // });
+
+  // describe("when MEETING_IDS includes event meeting_id", () => {
+  //   // should appendRowToGSheet (mock this)
+  //   // send SUCCESS 200
+  // });
+
+  // describe("when MEETING_IDS does not include event meeting_id", () => {
+  //   // throw new UnprocessableEntityError(
+  //   //   `MEETING ID IS NOT SUPPORTED: ${meetingId}`
+  //   // );
+  //   // send UNPROCESSABLE ENTITY 422
+  // });
+
+  // describe("when request auth header equals ZOOM_VERIFICATION_TOKEN", () => {
+  //   // should appendRowToGSheet (mock this)
+  //   // send SUCCESS 200
+  // });
+
+  // describe("when request auth header does not equal ZOOM_VERIFICATION_TOKEN", () => {
+  //   // throw new UnauthorizedRequestError("UNAUTHORIZED POST REQUEST");
+  //   // send FORBIDDEN 403
+  // });
 });
